c18c681a7fcd80e8406b80b8c476f4e363e4f5fc
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-11-11 23:51:30 +0800

    for deployment

--- lizardinteractiveonline/src/components/icons/index.ts
+++
@@ -1,7 +0,0 @@
-export { LizardInteractive } from "./svg.tsx";
-export { LizardIcon } from "./svg.tsx";
-export { Lizard } from "./svg.tsx";
-export { LizardTitle } from "./svg.tsx";
-export { Cover } from "./svg.tsx";
-export { LizardStroke } from "./svg.tsx";
-export { Center } from "./svg.tsx";
--- lizardinteractiveonline/src/lib/supabase.ts
+++
@@ -1,9 +0,0 @@
-// lib/supabase.ts
-
-import { createClient } from "@supabase/supabase-js";
-
-const supabaseUrl = import.meta.env.VITE_PUBLIC_SUPABASE_URL!;
-const supabaseKey = import.meta.env.VITE_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
-
