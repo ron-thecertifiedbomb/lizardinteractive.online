1dd0a82d4834f61b65c025a273e32b91ffbea2c1
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-11-15 05:29:09 +0800

    added nav logo

--- components/header.tsx
+++ components/header.tsx
@@ -5,10 +5,19 @@ export default function Header() {
   return (
     <header className="py-6 bg-dark-bg text-white">
       <Container>
-        <nav className="flex space-x-4">
-          <Link href="/">About</Link>
-          <Link href="/posts">Posts</Link>
-          <Link href="/blogs">Blog</Link>
+        <nav className="flex space-x-4 items-center justify-between">
+          <div className="flex justify-center items-center">
+            <img
+              src={"/lizardinteractive.png"}
+              alt={"lizardinteractive logo"}
+              className="max-w-xs max-h-12 rounded-xl"
+            /></div>
+       
+          <div className="flex gap-4">
+            <Link href="/">About</Link>
+            <Link href="/posts">Posts</Link>
+            <Link href="/blogs">Blog</Link></div>
+      
         </nav>
       </Container>
     </header>
