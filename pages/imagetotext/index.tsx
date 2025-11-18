bee7713452de827e1aaa5d098f4f286337743a6d
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2025-11-19 01:15:58 +0800

    for deployment

--- pages/imagetotext/index.tsx
+++ pages/imagetotext/index.tsx
@@ -15,14 +15,14 @@ export default function ImageToTextPage() {
                 <meta property="og:description" content="Quickly extract text from images with our easy-to-use online tool." />
                 <meta property="og:image" content={`${staticPreviewImage}?v=1`} />
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://www.lizardinteractive.online/imagetotext" />
+                <meta property="og:url" content="https://www.lizardinteractive.online/imagetotext.jpg" />
 
                 {/* Twitter */}
                 <meta name="twitter:card" content="summary_large_image" />
                 <meta name="twitter:title" content="Image to Text Converter | Lizard Interactive Online" />
                 <meta name="twitter:description" content="Quickly extract text from images with our easy-to-use online tool." />
                 <meta name="twitter:image" content={`${staticPreviewImage}?v=1`} />
-                <meta name="twitter:image" content="https://www.lizardinteractive.online/imagetotext" />
+                <meta name="twitter:image" content="https://www.lizardinteractive.online/imagetotext.jpg" />
             </Head>
             <SectionHeader
                 title="Image to Text Converter"
