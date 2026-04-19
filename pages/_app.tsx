ce0f92cef7008afc401d35411f79248f66430aeb
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2026-04-19 18:00:10 +0800

    improved seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,50 @@
 import "tailwindcss/tailwind.css";
 import '../styles/global.css'
 import type { AppProps } from "next/app";
+import Head from "next/head"; // Import Head
 import { Auth0Provider } from "@auth0/auth0-react";
 import { Analytics } from "@vercel/analytics/next"
 import Footer from "../components/Footer/Footer";
 import NavBar from "../components/NavBar/NavBar";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const siteTitle = "Lizard Interactive | RonDevSolutions & The Psychedelic Riffer";
+  const siteDescription = "Official hub for RonDevSolutions (Software Engineering) and The Psychedelic Riffer (Instrumental Metal).";
+  const siteUrl = "https://lizardinteractive.online";
+  const ogImage = `${siteUrl}/og-image-homepage.jpg`;
+
   return (
     <Auth0Provider
       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!}
       domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN!}
     >
+      <Head>
+        <title>{siteTitle}</title>
+        <meta name="description" content={siteDescription} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        {/* Open Graph / Facebook */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content={siteUrl} />
+        <meta property="og:title" content={siteTitle} />
+        <meta property="og:description" content={siteDescription} />
+        <meta property="og:image" content={ogImage} />
+        <meta property="fb:app_id" content="966242223397117" />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content={siteTitle} />
+        <meta name="twitter:description" content={siteDescription} />
+        <meta name="twitter:image" content={ogImage} />
+
+        <link rel="canonical" href={siteUrl} />
+      </Head>
 
       <div className="min-h-screen flex flex-col bg-black overflow-x-hidden relative">
-
-        {/* Fixed Navbar - stays on top */}
         <div className="fixed top-0 left-0 w-full z-[99999] pointer-events-auto">
           <NavBar />
         </div>
 
-        {/* Main content - pushes footer down naturally */}
         <main className="flex-1 flex flex-col w-full relative z-[10] pt-[72px] md:pt-[88px]">
           <Component {...pageProps} />
           <Analytics />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,58 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  const siteTitle = "Lizard Interactive | RonDevSolutions & The Psychedelic Riffer";
-  const siteDescription = "Official hub for RonDevSolutions (Software Engineering) and The Psychedelic Riffer (Instrumental Metal). High-performance web solutions and melodic guitar production.";
-  const siteUrl = "https://lizardinteractive.online";
-
-  // Primary OG image
-  const primaryOgImage = `${siteUrl}/og-image-homepage.jpg`;
-  // Fallback image (use your logo or a simple branded image)
-  const fallbackOgImage = `${siteUrl}/lizardinteractive.png`;
-
   return (
     <Html lang="en" className="bg-black">
       <Head>
+        {/* Core Settings */}
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
+        <meta name="theme-color" content="#000000" />
+        <meta name="format-detection" content="telephone=no" />
 
-        {/* SEO Foundation */}
-        <meta name="description" content={siteDescription} />
-        <meta name="author" content="RonDevSolutions" />
-        <link rel="canonical" href={siteUrl} />
-
-        {/* Facebook / Open Graph */}
-        <meta property="fb:app_id" content="966242223397117" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content={siteUrl} />
-        <meta property="og:title" content={siteTitle} />
-        <meta property="og:description" content={siteDescription} />
-        <meta property="og:image" content={primaryOgImage} />
-        <meta property="og:image:url" content={primaryOgImage} />
-        <meta property="og:image:secure_url" content={primaryOgImage} />
-        <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" />
-        <meta property="og:image:alt" content="Lizard Interactive - Code, Sound, and Vision" />
-        <meta property="og:site_name" content="Lizard Interactive" />
-        <meta property="og:locale" content="en_US" />
-
-        {/* Twitter */}
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content={siteTitle} />
-        <meta name="twitter:description" content={siteDescription} />
-        <meta name="twitter:image" content={primaryOgImage} />
-        <meta name="twitter:image:alt" content="Lizard Interactive - Code, Sound, and Vision" />
-
-        {/* Favicon & Branding */}
+        {/* Static Assets (Favicons) */}
         <link rel="icon" type="image/png" sizes="32x32" href="/lizardinteractive.png" />
         <link rel="apple-touch-icon" href="/lizardinteractive.png" />
 
-        {/* PWA / Mobile Theme Color */}
-        <meta name="theme-color" content="#000000" />
-
-        {/* Additional meta for better sharing */}
-        <meta name="format-detection" content="telephone=no" />
+        {/* Global SEO Defaults (Only if not handled in _app.tsx) */}
+        <meta name="robots" content="index, follow" />
       </Head>
 
       <body className="bg-black antialiased selection:bg-emerald-500 selection:text-black">
