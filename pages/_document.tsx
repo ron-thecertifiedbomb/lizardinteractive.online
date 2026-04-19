024bae938fadfcb45b4a1b35a2b9cf9a35ef3d89
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2026-04-19 20:33:42 +0800

    safe push

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,21 +1,10 @@
-// pages/_document.tsx
-import { Html, Head, Main, NextScript } from "next/document"; // Use this Head!
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en" className="bg-black" data-scroll-behavior="smooth">
-      <Head>
-        {/* DO NOT put <title> or <meta name="viewport"> here */}
-        <meta charSet="utf-8" />
-        <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta name="theme-color" content="#000000" />
-
-        {/* Static Assets Only */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/lizardinteractive.png" />
-        <link rel="apple-touch-icon" href="/lizardinteractive.png" />
-      </Head>
-
-      <body className="bg-black antialiased selection:bg-emerald-500 selection:text-black">
+      <Head />
+      <body className="bg-black antialiased">
         <Main />
         <NextScript />
       </body>
