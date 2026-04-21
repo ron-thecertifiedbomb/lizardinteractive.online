34fb44b9d130a99bb80c79557091e23bb94141d9
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2026-04-21 10:50:25 +0800

    preseentation section

--- pages/services/index.tsx
+++ pages/services/index.tsx
@@ -3,11 +3,14 @@
 import MetaHead from "@/components/MetaHead/MetaHead";
 import Hero from "@/components/shared/Hero/Hero";
 import Newsletter from "@/components/shared/NewsLetter/NewsLetter";
+import PresentationSection from "@/components/shared/PresentationSection/PresentationSection";
 import ScreenContainer from "@/components/shared/ScreenContainer/ScreenContainer";
 import ServicesCards from "@/components/shared/ServicesCards/ServicesCards";
 import { niches } from "@/data/lists/nichesList";
+import { landingPagePresentation } from "@/data/page/landingPagePresentation";
 
 import { servicesContent } from "@/data/servicesContent";
+import { Presentation } from "lucide-react";
 
 
 export default function ServicesPage() {
@@ -17,10 +20,13 @@ export default function ServicesPage() {
 
       <ScreenContainer variant="dark" maxWidth="xl" isHero={true}>
       <MetaHead data={seoEntry?.data} />
-      <Hero homeContent={servicesContent} />
-
-      <ServicesCards
-        niches={niches} />
+      {/* <Hero homeContent={servicesContent} /> */}
+      <PresentationSection
+        data={landingPagePresentation}
+        badge="Deployment Blueprint // v2.1"
+      />
+      {/* <ServicesCards
+        niches={niches} /> */}
       <Newsletter />
 
     </ScreenContainer>
