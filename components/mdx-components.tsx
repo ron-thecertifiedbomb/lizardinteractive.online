37b0c964ccde8ceb9713c8edbb5a461d1373839d
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2026-04-22 16:17:12 +0800

    safe checkpoint

--- components/mdx-components.tsx
+++
@@ -1,9 +0,0 @@
-import { MDXComponents } from 'mdx/types';
-import Image, { ImageProps } from 'next/image';
-
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-    return {
-        ...components,
-        img: (props) => <Image sizes="100vw" {...(props as ImageProps)} />
-    }
-};
