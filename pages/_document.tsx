55f0b1feab69638546dac4ea8ac8b663cc40d8c0
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2026-04-23 09:26:03 +0800

    dasdasdsad

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     < Html lang="en" suppressHydrationWarning>
-  
+      <Head />
       <body className="bg-black antialiased">
         <Main />
         <NextScript />
