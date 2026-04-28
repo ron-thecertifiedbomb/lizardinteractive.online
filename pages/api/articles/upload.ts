734dd6c7511498d129c2fd257d0d90bce83049f5
ron-thecertifiedbomb <ronan.sibunga@gmail.com>
2026-04-28 18:18:40 +0800

    for test

--- pages/api/articles/upload.ts
+++ pages/api/articles/upload.ts
@@ -1,10 +1,11 @@
 import { v2 as cloudinary } from "cloudinary";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Increase the body size limit to allow for base64 image uploads
